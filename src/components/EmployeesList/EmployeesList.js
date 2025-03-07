1502291481d4a11ae96e5e94a3128f9718851ecb
Nazar <nazarleochko2006@gmail.com>
2025-03-07 02:35:24 +0200

    Refactor project structure and remove unused files; add new components and styles for employee management

---
+++ src/components/EmployeesList/EmployeesList.js
@@ -0,0 +1,15 @@
+import EmployeesListItem from "../EmployeesListItem/EmployeesListItem";
+
+import './employees-list.css';
+
+const EmployeesList = () => {
+    return (
+        <ul className="app-list list-group">
+            <EmployeesListItem/>
+            <EmployeesListItem/>
+            <EmployeesListItem/>
+        </ul>
+    )
+}
+
+export default EmployeesList;
