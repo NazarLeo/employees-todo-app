02d462be5d70dd500734efcd13ac75fe74a99321
Nazar <nazarleochko2006@gmail.com>
2025-03-09 17:49:23 +0200

    Adding rise and premia by using state and refactored to Classes on Employees

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,6 +7,13 @@ import EmployeesAddForm from '../EmployeesAddForm/EmployeesAddForm';
 import './app.css';
 
 function App() {
+
+  const data = [
+    {name: 'Jacob Stone', salary: 800, increase: false, id: 1},
+    {name: 'David Depp', salary: 3000, increase: false, id: 2},
+    {name: 'Nazar Leochko', salary: 5000, increase: true, id: 3}
+  ]
+
   return (
     <div className="app">
         <AppInfo />
@@ -16,7 +23,7 @@ function App() {
             <AppFilter/>
         </div>
         
-        <EmployeesList/>
+        <EmployeesList data={data}/>
         <EmployeesAddForm/>
     </div>
   );
